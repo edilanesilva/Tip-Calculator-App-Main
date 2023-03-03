c1a01516cad0452fa969b272cc71dd1e9e063a91
edilanesilva <edilanesantos75@gmail.com>
2023-03-02 23:42:42 -0300

    js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
-var bill = document.querySelector("#bill");
-var btn5 = document.querySelector("#btn5");
-var btn10 = document.querySelector("#btn10");
-var btn15 = document.querySelector("#btn15");
-var btn25 = document.querySelector("#btn25");
-var btn50 = document.querySelector("#btn50");
-var btn50 = document.querySelector("#btn-custom");
-var numberOfPeople = document.querySelector("#qtd-people");
+const btn5 = document.querySelector('#btn5');
+const btn10 = document.querySelector('#btn10');
+const btn15 = document.querySelector('#btn15');
+const btn25 = document.querySelector('#btn25');
+const btn50 = document.querySelector('#btn50');
+const btnCustom =  document.querySelector('#btn-custom')
+const gorjetaPorPessoa = document.querySelector('#tip-amount');
+const totalGorjetaPorPessoa = document.querySelector('#total-person');
+
+function calculaGorjeta() {
+	const conta = document.querySelector('#bill').value
+	const numeroDePessoas = document.querySelector("#qtd-people").value
+	
+	if (conta == "" || numeroDePessoas == "") {
+		alert('Por favor, digite todos os dados')
+	}
+}
+
+
+
+
+
+
